14fd49f7884311a0a073e3cb2eb0d3354d016164
jjohnsundar <jjohnsundar9943@gmail.com>
2020-05-17 08:04:33 +0530

    Updates

---
+++ precache-manifest.57afa86fcfdab0bf44c1961962ad0423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dabc1349b33940d0c676aed6c41340d",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "981a00ad5813207e668d",
+    "url": "/Counter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "625d9421f551e2ccb35c",
+    "url": "/Counter/static/js/2.4906145d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Counter/static/js/2.4906145d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981a00ad5813207e668d",
+    "url": "/Counter/static/js/main.84607470.chunk.js"
+  },
+  {
+    "revision": "c1b55e5d6a4abf334a8e",
+    "url": "/Counter/static/js/runtime-main.8b2b3682.js"
+  }
+]);
